ac17d8d979b84bb9a3fc65751d93474575693013
palmerye <palmerye@gmail.com>
2020-04-03 23:29:13 +0800

    feat: 增加中间件promise包装

--- test/index.js
+++ test/index.js
@@ -2,8 +2,23 @@ const Koa = require('../lib/application')
 
 const app = new Koa()
 
-app.use((ctx, next) => {
-    ctx.body = 'hii'
+app.use(async (ctx, next) => {
+    console.log(1)
+    await next()
+    console.log(2)
+})
+app.use(async (ctx, next) => {
+    console.log(3)
+    let p = new Promise((resolve, roject) => {
+        setTimeout(() => {
+            console.log('3.5')
+            resolve()
+        }, 1000)
+    })
+    await p.then()
+    await next()
+    console.log(4)
+    ctx.body = 'hello world'
 })
 
 app.listen(3000)
